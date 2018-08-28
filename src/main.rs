e7fcce9b0f95bd5a230182e37861da76499d9156
chrisk_24 <chrisknappers@hotmail.com>
2018-08-28 10:37:10 -0400

    add sliding

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use graphics::*;
 
 pub struct Grid {
-    x_cells: i32,
-    y_cells: i32,
+    x_cells: u32,
+    y_cells: u32,
+    empty_x: u32,
+    empty_y: u32,
     cells: Vec<Cell>
 }
 
@@ -23,9 +25,9 @@ impl Grid {
                   t: &math::Matrix2d,
                   args: &RenderArgs)  {
         
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        clear([1.0,0.0,0.0,1.0], gl);
-        //self.cells.iter().map(|c| c.render(gl));
+        clear(BLACK, gl);
 
 
         for cell in &mut self.cells {
@@ -38,7 +40,6 @@ impl Grid {
     }
 
     pub fn update(&mut self) {
-        //self.cells.iter().map(|c| c.update());
 
         for c in &mut self.cells {
             c.update();
@@ -46,32 +47,102 @@ impl Grid {
     }
 
 
-    pub fn click(&mut self, x_cell: i32, y_cell: i32) {
-        println!("Clicked {},{}", x_cell, y_cell);
+    fn get_grid_index(x: u32, y: u32, w: u32) -> u32{
+        x + y * w
     }
 
-    pub fn new (x_cells: i32, y_cells: i32) -> Grid {
+
+    fn is_adjacent(A: (u32, u32), B: (u32, u32)) -> bool {
+        let (ax, ay) = A;
+        let (bx, by) = B;
+
+        ((ax as i32 - bx as i32).abs() == 1 &&
+         (ay == by)) ||
+        ((ay as i32 - by as i32).abs() == 1 &&
+         (ax == bx))
+    }
+
+    
+    fn swap_cells(&mut self, A: (u32, u32), B: (u32, u32)) {
+        let (ax, ay) = A;
+        let (bx, by) = B;
+        
+        let a_index = Grid::get_grid_index(ax,ay,self.x_cells);
+        let b_index = Grid::get_grid_index(bx,by,self.x_cells);
+
+        let a_cell_content = self.cells.get(a_index as usize).unwrap().content;
+        let b_cell_content = self.cells.get(b_index as usize).unwrap().content;
+
+        self.cells.get_mut(b_index as usize).unwrap().content = a_cell_content;
+        self.cells.get_mut(a_index as usize).unwrap().content = b_cell_content;
+    }
+    
+
+    pub fn click(&mut self, x_cell: u32, y_cell: u32) {
+        let index = Grid::get_grid_index(x_cell,y_cell,self.x_cells);
+        if let Some(cell) = self.cells.get_mut(index as usize) {
+            cell.click();
+        }
+
+        let (empty_x, empty_y) = (self.empty_x, self.empty_y);
+
+        //real code
+        if Grid::is_adjacent((x_cell, y_cell),
+                       (empty_x, empty_y)) {
+            println!("Clicked next to empty!");
+
+            self.swap_cells((x_cell, y_cell),
+                            (empty_x, empty_y));
+
+            self.empty_x = x_cell;
+            self.empty_y = y_cell;
+        }
+
+
+    }
+
+
+    pub fn new (x_cells: u32, y_cells: u32) -> Grid {
         let mut cells: Vec<Cell> = Vec::new();
+        for i in 0..x_cells*y_cells {
+            cells.push(Cell::new(0,0,None));
+        }
+
         for x in 0..x_cells {
             for y in 0..y_cells {
                 let col = (x+y) as f32 /
                           (x_cells + y_cells) as f32;
-                cells.push(Cell::new(x,y,Some(col)));
+                let index = Grid::get_grid_index(x,y,x_cells);
+                let cell = cells.get_mut(index as usize).unwrap();
+                cell.x_pos = x;
+                cell.y_pos = y;
+                cell.content = Some(col);
             }
-        }
-        
+        } 
+
+        let (empty_x, empty_y) = (x_cells - 1, y_cells - 1);
+
+        cells.get_mut(
+                Grid::get_grid_index(empty_x, 
+                                     empty_y, 
+                                     x_cells) 
+                    as usize)
+             .unwrap()
+             .content = None;
         
         Grid {
             x_cells: x_cells,
             y_cells: y_cells,
+            empty_x: empty_x,
+            empty_y: empty_y,
             cells: cells
         }
     }
 }
 
 pub struct Cell {
-    x_pos: i32, 
-    y_pos: i32,
+    x_pos: u32, 
+    y_pos: u32,
     content:Option<f32>
 }
 
@@ -81,8 +152,8 @@ impl Cell {
     pub fn render(&mut self, 
                   gl: &mut GlGraphics, 
                   t: &math::Matrix2d,
-                  x_cells: i32,
-                  y_cells: i32,
+                  x_cells: u32,
+                  y_cells: u32,
                   args: &RenderArgs) {
         let col: [f32; 4] = match self.content {
             Some(val) => [val,val,val, 1.0],
@@ -108,7 +179,11 @@ impl Cell {
 
     }
 
-    pub fn new(x: i32, y: i32, val: Option<f32>) -> Cell {
+    pub fn click(&mut self) {
+        println!("Clicked Cell ({}, {})", self.x_pos, self.y_pos);
+    }
+
+    pub fn new(x: u32, y: u32, val: Option<f32>) -> Cell {
         Cell {
             x_pos: x,
             y_pos: y,
@@ -147,8 +222,8 @@ impl App {
     pub fn click(&mut self, raw_x: f32, raw_y: f32, w: u32, h: u32) {
         let cell_width = w as f32 / self.grid.x_cells as f32;
         let cell_height = h as f32 / self.grid.y_cells as f32;
-        let cell_x = (raw_x as f32 / cell_width) as i32;
-        let cell_y = (raw_y as f32 / cell_height) as i32;
+        let cell_x = (raw_x as f32 / cell_width) as u32;
+        let cell_y = (raw_y as f32 / cell_height) as u32;
         self.grid.click(cell_x, cell_y);
     }
 
@@ -200,7 +275,10 @@ fn main() {
 
         if let Some(Button::Mouse(MouseButton::Left)) = e.press_args() {
               println!("Clicked {},{}", mx, my);
-              app.click(mx as f32,my as f32,window_width, window_height);
+              app.click(mx as f32,
+                        my as f32,
+                        window_width, 
+                        window_height);
 
         }
     }
